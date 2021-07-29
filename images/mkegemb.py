0073a651e45d90216648aadfee4aeaa0dd6b3627
joevt <joevt@shaw.ca>
2021-07-29 00:26:43 -0700

    Output name of unsupported mode
    So the user can better know what format change needs to be made to the input image.

--- images/mkegemb.py
+++ images/mkegemb.py
@@ -106,7 +106,7 @@ for filename in sys.argv[1:]:
             planes[2].append(pixeldata)
 
     else:
-        print (" Error: Mode not supported!!")
+        print (" Error: Mode \"%s\" not supported!!" % mode)
         continue
 
     # special treatment for fonts
